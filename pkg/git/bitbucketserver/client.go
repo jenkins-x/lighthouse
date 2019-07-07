2dd86fc4267d159f4a256fbd64257f21f32f07fa
Will Refvem 文仁 <wbrefvem@gmail.com>
2019-07-07 17:55:03 -0400

    Revert "Added CRD manifest, fixed issues with webhook creation"

--- pkg/git/bitbucketserver/client.go
+++ pkg/git/bitbucketserver/client.go
@@ -2,10 +2,9 @@ package bitbucketserver
 
 import (
 	apiv1 "github.com/jenkins-x/lighthouse/pkg/apis/jenkins.io/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// Provider is an implementation of the git.Provider interface for Bitbucket Server
+// BitbucketServer is an implementation of the git.Provider interface
 type Provider struct {
 	URL  string
 	Name string
@@ -14,9 +13,6 @@ type Provider struct {
 // ParseWebhook encapsulates provider-specific logic for creating a Webhook CRD
 func (bbs *Provider) ParseWebhook(rawWebhook []byte) *apiv1.Webhook {
 	return &apiv1.Webhook{
-		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: "webhook-",
-		},
 		Spec: apiv1.WebhookSpec{
 			EventType: "pull_request",
 			Ref:       "abc123",
