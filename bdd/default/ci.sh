dc4c25527924360cc0569bcb9458a5610f31edf2
Will Refvem <wbrefvem@gmail.com>
2019-11-15 14:47:43 -0500

    fix(bdd): correct path to cluster.yaml

--- bdd/default/ci.sh
+++ bdd/default/ci.sh
@@ -51,7 +51,7 @@ jx step bdd \
     --use-revision \
     --version-repo-pr \
     --versions-repo https://github.com/jenkins-x/jenkins-x-versions.git \
-    --config ../jx/bdd/boot-lh/cluster.yaml \
+    --config ../bdd/default/cluster.yaml \
     --gopath /tmp \
     --git-provider=github \
     --git-username $GH_USERNAME \
