e8710d6b6bb7bd85a06ec259091cdba3db258c39
Andrew Bayer <andrew.bayer@gmail.com>
2019-11-27 16:31:32 +0100

    chore: Update ci.shs for BDD tests to continue to work right
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- bdd/ghe/ci.sh
+++ bdd/ghe/ci.sh
@@ -11,9 +11,15 @@ export BUILD_NUMBER="$BUILD_ID"
 JX_HOME="/tmp/jxhome"
 KUBECONFIG="/tmp/jxhome/config"
 
-mkdir -p $JX_HOME
+# lets avoid the git/credentials causing confusion during the test
+export XDG_CONFIG_HOME=$JX_HOME
+
+mkdir -p $JX_HOME/git
 
 jx --version
+
+# replace the credentials file with a single user entry
+echo "https://$GH_USERNAME:$GH_ACCESS_TOKEN@github.beescloud.com" > $JX_HOME/git/credentials
 
 gcloud auth activate-service-account --key-file $GKE_SA
 
