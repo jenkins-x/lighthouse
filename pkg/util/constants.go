8a77171f60d0f0007a2699ad380b1adcfaf5d0ae
Andrew Bayer <andrew.bayer@gmail.com>
2020-03-08 14:48:37 -0400

    fix: Use x-access-token as username in git client with GHA
    See https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#http-based-git-access-by-an-installation
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- pkg/util/constants.go
+++ pkg/util/constants.go
@@ -6,4 +6,8 @@ const (
 
 	// OverriddenByPrefix is the beginning of the description for commit statuses set by /override
 	OverriddenByPrefix = "Overridden by"
+
+	// GitHubAppGitRemoteUsername Username for git https URLs when using a GitHub App token.
+	// see https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#http-based-git-access-by-an-installation
+	GitHubAppGitRemoteUsername = "x-access-token"
 )
