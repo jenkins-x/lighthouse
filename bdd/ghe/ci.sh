ee57092320ea1fdae39ec4ba625bfce5de6b7ed0
Andrew Bayer <andrew.bayer@gmail.com>
2020-03-26 13:26:40 -0400

    chore: Remove app lifecycle tests
    These are actually redundant now that we have `test-lighthouse`. And
    let's use `test-create-spring` because we want to get the merge all
    the way to prod.
    
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- bdd/ghe/ci.sh
+++ bdd/ghe/ci.sh
@@ -85,10 +85,8 @@ jx step bdd \
     --no-delete-app \
     --no-delete-repo \
     --tests install \
-    --tests test-quickstart-golang-http \
-    --tests test-lighthouse \
-    --tests test-app-lifecycle
-
+    --tests test-create-spring \
+    --tests test-lighthouse
 
 bdd_result=$?
 if [[ $bdd_result != 0 ]]; then
