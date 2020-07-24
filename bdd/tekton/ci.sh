53f3c2199dad1f99aeb539b8aac6306522206658
Andrew Bayer <andrew.bayer@gmail.com>
2020-07-24 13:30:12 -0400

    wip: sleep, better hmac generation, comments
    Signed-off-by: Andrew Bayer <andrew.bayer@gmail.com>

--- bdd/tekton/ci.sh
+++ bdd/tekton/ci.sh
@@ -2,32 +2,54 @@
 set -e
 set -x
 
+# Activate the service account json provided by the pipeline
 gcloud auth activate-service-account --key-file $GKE_SA
 
-gcloud container clusters create lh-tekton-e2e-${BUILD_ID} --num-nodes=3 --machine-type n1-standard-2 --enable-autoscaling --min-nodes=3 --max-nodes=5 --zone=europe-west1-c --scopes=https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.full_control,https://www.googleapis.com/auth/service.management,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring --labels=created-by=$(id -u -n),create-time=$(date "+%a-%b-%d-%Y-%H-%M-%S" |tr '[:upper:]' '[:lower:]'),cluster=lh-tekton-e2e,branch=$(echo $BRANCH_NAME | tr '[:upper:]' '[:lower:]') --project=jenkins-x-bdd3
+if [ -z "${USER}" ]; then
+  USER=$(id -u -n)
+fi
 
+if [ -z "${BRANCH_NAME}" ]; then
+  BRANCH_NAME=local
+fi
+
+if [ -z "${BUILD_ID}" ]; then
+  BUILD_ID=$(tr -dc '0-9' < /dev/urandom | head -c5)
+fi
+
+# Create the cluster with some standard labels and info for cleanup. Minimum version is 1.16.x
+gcloud container clusters create lh-tekton-e2e-${BUILD_ID} --num-nodes=3 --machine-type n1-standard-2 --enable-autoscaling --min-nodes=3 --max-nodes=5 --zone=europe-west1-c --scopes=https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.full_control,https://www.googleapis.com/auth/service.management,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring --labels=created-by=${USER},create-time=$(date "+%a-%b-%d-%Y-%H-%M-%S" |tr '[:upper:]' '[:lower:]'),cluster=lh-tekton-e2e,branch=$(echo $BRANCH_NAME | tr '[:upper:]' '[:lower:]') --project=jenkins-x-bdd3 --cluster-version=1.16.9-gke.6
+
+# Install the nginx ingress controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.34.1/deploy/static/provider/cloud/deploy.yaml
 
 # Sleep 30 seconds to wait for the ingress controller to get an external IP and then capture it
+sleep 30
 
+# Capture the external IP since it'll end up getting used in our "domain name"
 EXTERNAL_IP=$(kubectl get svc -n ingress-nginx ingress-nginx-controller -o jsonpath={.status.loadBalancer.ingress[0].ip})
 
+# Create our test namespace and switch into it
 kubectl create namespace lh-test
-
 kubectl config set-context --current --namespace=lh-test
 
-curl https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml | sed -E "s/namespace\: tekton-pipelines/namespace: lh-test/" > install-tekton.yml
-
+# Download the Tekton v0.14.2 release YAML, switch the namespace in it, and apply it.
+curl https://storage.googleapis.com/tekton-releases/pipeline/previous/v0.14.2/release.yaml | sed -E "s/namespace\: tekton-pipelines/namespace: lh-test/" > install-tekton.yml
 kubectl apply -f install-tekton.yml
 
-export E2E_HMAC_TOKEN=$(openssl rand -hex 21)
+# HMAC token is just a random 42 byte hex string we'll be using in Lighthosue and the webhook
+export E2E_HMAC_TOKEN=$(tr -dc 'A-F0-9' < /dev/urandom | head -c42)
 
+# Take the template for values and replace the various placeholders - this could probably be cleaner.
 cat bdd/tekton/values.yaml.template | sed 's/$VERSION/'"$VERSION"'/' | sed 's/$BOTUSER/'"$E2E_GIT_USER"'/' | sed 's/$HMACTOKEN/'"$E2E_HMAC_TOKEN"'/' | sed 's/$BOTSECRET/'"$E2E_PRIMARY_SCM_TOKEN"'/' | sed 's/$DOMAINNAME/'"$EXTERNAL_IP"'/' > myvalues.yaml
 
+# helm 3 is installed on jx builders as "helm3", and that's what we want to use to install here.
 helm3 install -f myvalues.yaml --namespace lh-test lighthouse charts/lighthouse
 
+# set some other variables we're going to need in the e2e tests.
 export E2E_GIT_SERVER=https://github.com
 export E2E_GIT_KIND=github
 
+# Run the test - we probably want something here to capture controller logs but we'll deal with that in a bit.
 make run-e2e-tests
 
