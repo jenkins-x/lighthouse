993a141f9fb0b540c5b69a8212538c0fc6242012
Hardy Ferentschik <hardy@ferentschik.de>
2020-08-17 13:18:00 +0200

    fix: allow to run binaries locally
    - explicitly include k8s.io/client-go/plugin/pkg/client/auth
    - set Kubernetes dependencies to v0.17.6 (only one replace required for client-go)
    - add go_mod_tidy_hack in order to fix go-autorest go module issues when changing client-go dependency
    
    fixes #928

---
+++ pkg/util/go_mod_tidy_hack.go
@@ -0,0 +1,8 @@
+// +build modhack
+
+package util
+
+// Necessary for safely adding multi-module repo. See: https://github.com/golang/go/wiki/Modules#is-it-possible-to-add-a-module-to-a-multi-module-repository
+import _ "github.com/Azure/go-autorest"
+
+// This file, and the github.com/Azure/go-autorest import, won't actually become part of  the resultant binary.
