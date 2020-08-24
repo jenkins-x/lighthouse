a1e0be9a5b1f008c3a4d6c698c850b03b8fc6018
charles-edouard.breteche <charles.breteche@agriconomie.com>
2020-08-24 16:39:15 +0200

    chore: replace clients.GetConfig with clientcmd.BuildConfigFromFlags

--- pkg/clients/clients.go
+++ pkg/clients/clients.go
@@ -1,11 +1,6 @@
 package clients
 
 import (
-	"fmt"
-	"os"
-	"os/user"
-	"path/filepath"
-
 	clientset "github.com/jenkins-x/lighthouse/pkg/client/clientset/versioned"
 	"github.com/pkg/errors"
 	"k8s.io/client-go/rest"
@@ -20,7 +15,7 @@ import (
 
 // GetAPIClients returns the tekton, kube, and Lighthouse clients and the kubeconfig used to create them
 func GetAPIClients() (tektonclient.Interface, kubeclient.Interface, clientset.Interface, *rest.Config, error) {
-	kubeCfg, err := GetConfig("", "")
+	kubeCfg, err := clientcmd.BuildConfigFromFlags("", "")
 	if err != nil {
 		return nil, nil, nil, nil, errors.Wrap(err, "unable to get kubeconfig")
 	}
@@ -41,32 +36,4 @@ func GetAPIClients() (tektonclient.Interface, kubeclient.Interface, clientset.In
 	}
 
 	return tektonClient, kubeClient, lhClient, kubeCfg, nil
-}
-
-// GetConfig returns a rest.Config to be used for kubernetes client creation.
-// It does so in the following order:
-//   1. Use the passed kubeconfig/masterURL.
-//   2. Fallback to the KUBECONFIG environment variable.
-//   3. Fallback to in-cluster config.
-//   4. Fallback to the ~/.kube/config.
-func GetConfig(masterURL, kubeconfig string) (*rest.Config, error) {
-	if kubeconfig == "" {
-		kubeconfig = os.Getenv("KUBECONFIG")
-	}
-	// If we have an explicit indication of where the kubernetes config lives, read that.
-	if kubeconfig != "" {
-		return clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
-	}
-	// If not, try the in-cluster config.
-	if c, err := rest.InClusterConfig(); err == nil {
-		return c, nil
-	}
-	// If no in-cluster config, try the default location in the user's home directory.
-	if usr, err := user.Current(); err == nil {
-		if c, err := clientcmd.BuildConfigFromFlags("", filepath.Join(usr.HomeDir, ".kube", "config")); err == nil {
-			return c, nil
-		}
-	}
-
-	return nil, fmt.Errorf("could not create a valid kubeconfig")
 }
