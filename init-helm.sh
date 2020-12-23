9b8dbb9ec0933a85f3502ca492f31231a04995d5
James Strachan <james.strachan@gmail.com>
2020-12-23 13:31:24 +0000

    fix: try fix release pipeline

--- init-helm.sh
+++ init-helm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-helm init --client-only --skip-refresh
-helm repo rm stable
-helm repo add stable https://charts.helm.sh/stable
+helm init --client-only --skip-refresh --stable-repo-url https://charts.helm.sh/stable
+#helm repo rm stable
+#helm repo add stable https://charts.helm.sh/stable
