1279012e8a5a22c7688b4de47c21a30891bc0d73
Cameron Braid <cameron@drivenow.com.au>
2021-02-11 00:42:20 +1100

    fix: in uses resolver, clone pipelinerun when returning from the cache to avoid changes persistng between evaluations

--- pkg/triggerconfig/inrepo/load_pipelinerun_test.go
+++ pkg/triggerconfig/inrepo/load_pipelinerun_test.go
@@ -1,6 +1,7 @@
 package inrepo
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -10,6 +11,7 @@ import (
 	"github.com/jenkins-x/go-scm/scm/driver/fake"
 	"github.com/jenkins-x/lighthouse/pkg/filebrowser"
 	"github.com/jenkins-x/lighthouse/pkg/scmprovider"
+	"github.com/jenkins-x/lighthouse/pkg/util"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -33,6 +35,8 @@ func TestLoadPipelineRunTest(t *testing.T) {
 
 	// lets use a custom version stream sha
 	os.Setenv("LIGHTHOUSE_VERSIONSTREAM_JENKINS_X_JX3_PIPELINE_CATALOG", "myversionstreamref")
+
+	require.NoError(t, err, "failed to get cwd")
 
 	// make it easy to run a specific test only
 	runTestName := os.Getenv("TEST_NAME")
@@ -65,37 +69,55 @@ func TestLoadPipelineRunTest(t *testing.T) {
 		dir := filepath.Join(sourceDir, name)
 		resolver.Dir = dir
 
-		path := filepath.Join(dir, "source.yaml")
-		expectedPath := filepath.Join(dir, "expected.yaml")
+		i := 0
+		for i <= 10 {
+			i++
 
-		message := "load file " + path
-		data, err := ioutil.ReadFile(path)
-		require.NoError(t, err, "failed to load "+message)
+			suffix := ""
+			if i > 1 {
+				suffix = fmt.Sprintf("%v", i)
+			}
+			path := filepath.Join(dir, fmt.Sprintf("source%s.yaml", suffix))
 
-		pr, err := LoadTektonResourceAsPipelineRun(resolver, data)
-		if strings.HasSuffix(name, "-fails") {
-			require.Errorf(t, err, "expected failure for test %s", name)
-			t.Logf("test %s generated expected error %s\n", name, err.Error())
-			continue
+			exists, err := util.FileExists(path)
+			require.NoError(t, err, "failed to check for file exists source "+path)
+
+			if !exists && i > 1 {
+				break
+			}
+
+			expectedPath := filepath.Join(dir, fmt.Sprintf("expected%s.yaml", suffix))
+
+			message := "load file " + path
+			data, err := ioutil.ReadFile(path)
+			require.NoError(t, err, "failed to load "+message)
+
+			pr, err := LoadTektonResourceAsPipelineRun(resolver, data)
+
+			if strings.HasSuffix(name, "-fails") {
+				require.Errorf(t, err, "expected failure for test %s", name)
+				t.Logf("test %s generated expected error %s\n", name, err.Error())
+				continue
+			}
+
+			require.NoError(t, err, "failed to load PipelineRun for "+message)
+			require.NotNil(t, pr, "no PipelineRun for "+message)
+
+			data, err = yaml.Marshal(pr)
+			require.NoError(t, err, "failed to marshal generated PipelineRun for "+message)
+
+			if generateTestOutput {
+				err = ioutil.WriteFile(expectedPath, data, 0666)
+				require.NoError(t, err, "failed to save file %s", expectedPath)
+				continue
+			}
+			expectedData, err := ioutil.ReadFile(expectedPath)
+			require.NoError(t, err, "failed to load file "+expectedPath)
+
+			text := strings.TrimSpace(string(data))
+			expectedText := strings.TrimSpace(string(expectedData))
+
+			assert.Equal(t, expectedText, text, "PipelineRun loaded for "+message)
 		}
-
-		require.NoError(t, err, "failed to load PipelineRun for "+message)
-		require.NotNil(t, pr, "no PipelineRun for "+message)
-
-		data, err = yaml.Marshal(pr)
-		require.NoError(t, err, "failed to marshal generated PipelineRun for "+message)
-
-		if generateTestOutput {
-			err = ioutil.WriteFile(expectedPath, data, 0666)
-			require.NoError(t, err, "failed to save file %s", expectedPath)
-			continue
-		}
-		expectedData, err := ioutil.ReadFile(expectedPath)
-		require.NoError(t, err, "failed to load file "+expectedPath)
-
-		text := strings.TrimSpace(string(data))
-		expectedText := strings.TrimSpace(string(expectedData))
-
-		assert.Equal(t, expectedText, text, "PipelineRun loaded for "+message)
 	}
 }
