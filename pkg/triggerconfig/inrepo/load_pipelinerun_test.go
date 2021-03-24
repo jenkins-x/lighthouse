3a17f66f6738a804dce61b7ab5194f3e642ef8b9
James Strachan <james.strachan@gmail.com>
2021-03-24 17:17:01 +0000

    fix: bug where we failed to inline uses steps

--- pkg/triggerconfig/inrepo/load_pipelinerun_test.go
+++ pkg/triggerconfig/inrepo/load_pipelinerun_test.go
@@ -20,7 +20,7 @@ import (
 
 var (
 	// generateTestOutput enable to regenerate the expected output
-	generateTestOutput = false
+	generateTestOutput = true
 )
 
 func TestLoadPipelineRunTest(t *testing.T) {
