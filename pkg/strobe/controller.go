3001c1bf384351afc04d08905d2b3955bf8e02d2
Luke Addison <lukeaddison785@gmail.com>
2022-09-29 18:09:35 +0100

    Improve lock explanation

--- pkg/strobe/controller.go
+++ pkg/strobe/controller.go
@@ -106,9 +106,9 @@ func (c *LighthousePeriodicJobController) findLighthousePeriodicJobConfig(req ct
 func generateLighthouseJob(logger *logrus.Entry, periodicJobConfig *job.Periodic, lastMissedScheduleTime time.Time) *v1alpha1.LighthouseJob {
 	// We generate a deterministic LighthouseJob name using the last missed
 	// schedule time. This acts as a lock to prevent duplicate LighthouseJobs
-	// from being created for the same schedule time. This also allows us to
-	// find the LighthouseJob if the controller crashes after creating it but
-	// before updating its status to trigger it. Inspired by:
+	// from being created for the same schedule time during a rolling update.
+	// This also allows us to find the LighthouseJob if the controller crashes
+	// after creating it but before triggering it. Inspired by:
 	// https://github.com/kubernetes/kubernetes/blob/v1.24.6/pkg/controller/cronjob/utils.go#L181-L184
 	suffix := fmt.Sprintf("-%d", lastMissedScheduleTime.Unix()/60)
 	// Kubernetes resource names have a maximum length of 253:
