d07ee48b27f410e98c1ddf4695477e36e8d63a9f
Luke Addison <lukeaddison785@gmail.com>
2022-11-25 16:58:56 +0000

    Improve logic

--- pkg/util/scmclient.go
+++ pkg/util/scmclient.go
@@ -182,15 +182,18 @@ func getSCMTokenFromPath(gitKind string) (string, error) {
 // HMACToken gets the HMAC token from the environment or the filesystem
 func HMACToken() string {
 	hmacToken := os.Getenv("HMAC_TOKEN")
-	// If HMAC_TOKEN_PATH is specified then attempt to read from the filesystem
-	hmacTokenPath := os.Getenv("HMAC_TOKEN_PATH")
-	if len(hmacToken) == 0 && len(hmacTokenPath) > 0 {
-		b, err := os.ReadFile(hmacTokenPath)
-		if err != nil {
-			logrus.Errorf("failed to read HMAC_TOKEN_PATH %s: %s", hmacTokenPath, err)
-			return hmacToken
+	// Prioritise the value from the environment for backwards compatibility
+	if len(hmacToken) == 0 {
+		// If HMAC_TOKEN_PATH is specified then attempt to read from the filesystem
+		hmacTokenPath := os.Getenv("HMAC_TOKEN_PATH")
+		if len(hmacTokenPath) > 0 {
+			b, err := os.ReadFile(hmacTokenPath)
+			if err != nil {
+				logrus.Errorf("failed to read HMAC_TOKEN_PATH %s: %s", hmacTokenPath, err)
+				return hmacToken
+			}
+			hmacToken = string(b)
 		}
-		hmacToken = string(b)
 	}
 	return hmacToken
 }
