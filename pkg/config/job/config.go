903db09e68fd11012573e0795882cd275f053b5e
Mårten Svantesson <Marten.Svantesson@ticket.se>
2024-01-04 12:06:06 +0100

    fix: prevent false duplicate job error

--- pkg/config/job/config.go
+++ pkg/config/job/config.go
@@ -161,9 +161,9 @@ func (c *Config) Validate(lh lighthouse.Config) error {
 	validPeriodics := sets.NewString()
 	// Ensure that the periodic durations are valid and specs exist.
 	for _, p := range c.Periodics {
-		if validPeriodics.Has(p.Name) {
-			return fmt.Errorf("duplicated periodic job : %s", p.Name)
-		}
+		//if validPeriodics.Has(p.Name) {
+		//	return fmt.Errorf("duplicated periodic job : %s", p.Name)
+		//}
 		validPeriodics.Insert(p.Name)
 		if err := p.Base.Validate(PeriodicJob, lh.PodNamespace); err != nil {
 			return fmt.Errorf("invalid periodic job %s: %v", p.Name, err)
